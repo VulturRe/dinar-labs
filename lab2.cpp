cd683ae6e0435e2f7fdd4ee1306ab78087008c79
Ilshat Galimov <ilshatgalimovf@gmail.com>
2017-04-21 21:59:22 +0300

    Second lab is done

--- lab2.cpp
+++ lab2.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+unsigned keyUsageCount(vector<int> list, int key) {
+    unsigned count = 0;
+    for (auto v : list)
+        if (v == key) count++;
+    return count;
+}
 
 int main() {
     setlocale(LC_ALL, "rus");
+
+    vector<int> list;
+    unsigned count;
+    int key;
+
+    cout << "Введите количество элементов списка.\n";
+    cin >> count;
+    cout << "\nВведите элементы списка.\n";
+    for (unsigned i = 0; i < count; i++) {
+        int temp;
+        cin >> temp;
+        list.push_back(temp);
+    }
+
+    cout << "\nВведите ключ для поиска.\n";
+    cin >> key;
+    cout << "\nКоличество вхождений ключа в список = " <<
+         keyUsageCount(list, key) << endl;
 
     return 0;
 }
